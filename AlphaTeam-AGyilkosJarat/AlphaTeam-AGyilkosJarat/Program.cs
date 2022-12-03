9f773870f385e9737c9fd342a2ade07b515a2e24
Csapii <mail.szasz.csaba@gmail.com>
2022-12-03 12:30:19 +0100

    playing loop + moving function(up, down, left, right)

--- AlphaTeam-AGyilkosJarat/AlphaTeam-AGyilkosJarat/Program.cs
+++ AlphaTeam-AGyilkosJarat/AlphaTeam-AGyilkosJarat/Program.cs
@@ -10,49 +10,124 @@ namespace AlphaTeam_AGyilkosJarat
     {
         static void Main(string[] args)
         {
-
+            //set window size
             Console.SetWindowSize(80, 25);
 
+            //wagon[NxN] field, mainly (will be) odd numbers to get nice field
             int wagon1_row = 7;
             int wagon1_col = 7;
             int[,] wagon1 = new int[wagon1_row, wagon1_col];
 
-            for (int i = 0; i < wagon1.GetLength(0); i++)
+            int player_pos_y = (wagon1_row - 1) / 2;
+            int player_pos_x = 1;
+
+            bool playing = true;
+
+            while (playing)
             {
-                for (int j = 0; j < wagon1.GetLength(1); j++)
+                for (int i = 0; i < wagon1.GetLength(0); i++)
                 {
-                    //middle path
-                    if (i == (wagon1_row - 1) / 2 && j > wagon1_col-wagon1_col)
+                    for (int j = 0; j < wagon1.GetLength(1); j++)
                     {
-                        Console.Write("-");
-                        Console.Write("\t");
-                        
-                    }
-                    //chairs
-                    else if ((i > wagon1_row-wagon1_row && i < wagon1_row-1) && (j < wagon1_col-1 && j > wagon1_col-wagon1_col))
-                    {
-                        Console.Write("L");
-                        Console.Write("\t");
-                    }
-                    //around
-                    else
-                    {
-                        //adding more '#' at exit side
-                        if ((i == ((wagon1_row - 1) / 2) - 1 || i == ((wagon1_row - 1) / 2) + 1) && j == wagon1_col-1)
+                        //print player
+                        if (i == player_pos_y && j == player_pos_x)
                         {
-                            Console.Write("#######");
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write("@");
+                            Console.ResetColor();
+                            Console.Write("\t");
                         }
                         else
                         {
-                            Console.Write($"#");
-                            Console.Write("\t");
+                            //middle path
+                            if (i == (wagon1_row - 1) / 2 && j > wagon1_col - wagon1_col)
+                            {
+
+                                Console.Write("-");
+                                Console.Write("\t");
+
+                            }
+                            //chairs
+                            else if ((i > wagon1_row - wagon1_row && i < wagon1_row - 1) && (j < wagon1_col - 1 && j > wagon1_col - wagon1_col))
+                            {
+                                Console.Write("L");
+                                Console.Write("\t");
+                            }
+                            //around
+                            else
+                            {
+                                //adding more '#' at exit side
+                                if ((i == ((wagon1_row - 1) / 2) - 1 || i == ((wagon1_row - 1) / 2) + 1) && j == wagon1_col - 1)
+                                {
+                                    Console.Write("#######");
+                                }
+                                else
+                                {
+                                    Console.Write($"#");
+                                    Console.Write("\t");
+                                }
+                            }
                         }
+                        
+
                     }
-                    
+                    Console.WriteLine();
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-                Console.WriteLine();
+
+                Console.WriteLine("\n\n[1]-Fel\n[2]-Le\n[3]-Jobbra\n[4]-Balra\n\n[8]-Kilépés\n");
+                Console.Write("Válasz: ");
+                int player_move = int.Parse(Console.ReadLine());
+
+                //player moving (up, down, left, right)
+                //player can't go through the "walls"
+                if (player_move == 1 && player_pos_x != wagon1_col - 1) //up
+                {
+                    //won't allow to go up if the player pos y and x in the middle path at the exit pos
+                    if (player_pos_y != wagon1_row-wagon1_row+1)
+                    {
+                        player_pos_y--;
+                    }
+                }
+                else if (player_move == 2) //down
+                {
+                    //won't allow to go down if the player pos y and x in the middle path at the exit pos
+                    if (player_pos_y != wagon1_row-2 && player_pos_x != wagon1_col - 1)
+                    {
+                        player_pos_y++;
+                    }
+                }
+                else if (player_move == 3) //right
+                {
+                    //allowing the exit position (last '_' char on the playing field)
+                    if (player_pos_y == (wagon1_row - 1) / 2 && player_pos_x != wagon1_col - 1)
+                    {
+                        player_pos_x++;
+                    }
+                    else if (player_pos_y != (wagon1_row - 1) / 2 && player_pos_x != wagon1_col - 2)
+                    {
+                        player_pos_x++;
+                    }
+
+                }
+                else if (player_move == 4) //left
+                {
+                    if (player_pos_x != wagon1_col - wagon1_col + 1)
+                    {
+                        player_pos_x--;
+                    }
+
+
+                }
+                else if (player_move == 8) //exit
+                {
+                    playing = false;
+                }
+
+                //clearing the old stuff
+                Console.Clear();
             }
+            
 
         }
     }
